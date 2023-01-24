42606e199917d619e5edc56877d198db08b9b458
llamm-de <lamm.lukas@googlemail.com>
2023-01-24 21:19:51 +0100

    added custom activation functions

--- src/activations.py
+++ src/activations.py
@@ -14,8 +14,20 @@ def activation_exp(x):
     """
     return tf.math.exp(x)
 
+def activation_squared(x):
+    """
+    Squared activation function 
+    """
+    return tf.math.pow(x,2.0)
+
+def activation_ln_custom(x):
+    """
+    Custom logarithmic activation function 
+    """
+    return -1.0 * tf.math.log(1.0 - x)
+
 def activation_ln(x):
     """
     Logarithmic activation function 
     """
-    return -1.0 * tf.math.log(1.0 - x)
+    return tf.math.log(x)
--- tests/test_activations.py
+++ tests/test_activations.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.activations import activation_exp, activation_exp_min_one, activation_ln
+from src.activations import activation_exp, activation_exp_min_one, activation_ln_custom, activation_ln, activation_squared
 import tensorflow as tf
 
 class TestActivations(unittest.TestCase):
@@ -13,8 +13,15 @@ class TestActivations(unittest.TestCase):
         self.assertEqual(activation_exp(0.0), 1.0)
         self.assertEqual(activation_exp(1.0), tf.math.exp(1.0))
 
+    def test_activation_ln_custom(self):
+        self.assertEqual(activation_ln_custom(0.0), 0.0)
+        self.assertEqual(activation_ln_custom(1.0), -1.0 * tf.math.log(0.0))
+
     def test_activation_ln(self):
-        self.assertEqual(activation_ln(0.0), 0.0)
-        self.assertEqual(activation_ln(1.0), -1.0 * tf.math.log(0.0))
+        self.assertEqual(activation_ln(1.0), 0.0)
+        self.assertEqual(activation_ln(2.0), tf.math.log(2.0))
 
-
+    def test_activation_squared(self):
+        self.assertEqual(activation_squared(0.0), 0.0)
+        self.assertEqual(activation_squared(2.0), 4.0)
+        self.assertEqual(activation_squared(-2.0), 4.0)
