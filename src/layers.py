b1bc709ff4d832f85cf25f5ff9ba530a9a8f3a22
Lukas <lamm@ifam.rwth-aachen.de>
2023-01-27 23:17:48 +0100

    added PsiIsoBlock

--- src/layers.py
+++ src/layers.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 from tensorflow import keras
 
 from .mechanics import IsochoricVolumetricSplit, Invariants, RightCauchyGreen
+from .activations import activation_exp
 
 
 class MechanicsPreprocessBlock(keras.layers.Layer):
     """
     Block for preprocessing of mechanics. First apply isochoric-volumetric
-    split. Next, calculate the right Cauchy Green tensor and its invariants.
+    split. Next, calculate the isochoric right Cauchy Green tensor and its invariants.
 
     Inputs:
         def_grad - Deformation gradient (mechanical part)
@@ -25,36 +26,65 @@ class MechanicsPreprocessBlock(keras.layers.Layer):
         right_cauchy_green_iso = RightCauchyGreen()(def_grad_iso)
         I1, I2 = Invariants()(right_cauchy_green_iso)
         return I1, I2, J
-        
 
 
-class FunctionalBlock(keras.layers.Layer):
+class PsiIsoBlock(keras.layers.Layer):
     """
-    Custom functional layer for transformation of powers of invariants 
-    using designated activation functions.
+    Blog for computing isotropic strain energy density from invariants of 
+    isochoric right Cauchy Green tensor.
+
+    Inputs:
+        I1 - First invariant of isochoric right Cauchy Green tensor
+        I2 - Second invariant of isochoric right Cauchy Green tensor
+
+    Outputs:
+        PsiIso - Isochoric strain energy function
     """
-    def __init__(self, identity_initializer, exponential_initializer, l2_factor, layer_id, node_id) -> None:
+    def __init__(self, max_power=2, l2_factor = 0.001) -> None:
         super().__init__()
-        self.identity = keras.layers.Dense(1,
-                                  kernel_initializer=identity_initializer,
-                                  kernel_constraint=keras.constraints.NonNeg(),
-                                  kernel_regularizer=keras.regularizers.l2(l2_factor),
-                                  use_bias=False, 
-                                  activation=None,
-                                  name=f"w{layer_id}{2*node_id-1}")
-        self.exponential = keras.layers.Dense(1,
-                                     kernel_initializer=exponential_initializer,
-                                     kernel_constraint=keras.constraints.NonNeg(),
-                                     kernel_regularizer=keras.regularizers.l2(l2_factor),
-                                     use_bias=False, 
-                                     activation=activation_exp_min_one,
-                                     name=f"w{layer_id}{2*node_id}")
+        self.powers = tf.range(1,max_power+1,dtype='float32')
+        self.w_identity = self.add_weight(shape = (2*max_power,), 
+                                 initializer = keras.initializers.GlorotNormal(), 
+                                 constraint = tf.keras.constraints.NonNeg(), 
+                                 regularizer = keras.regularizers.l2(l2_factor),
+                                 trainable = True)                         
+        self.w_exp = self.add_weight(shape = (2*max_power,), 
+                                 initializer = keras.initializers.RandomUniform(minval=0.0, maxval=0.00001), 
+                                 constraint = tf.keras.constraints.NonNeg(), 
+                                 regularizer = keras.regularizers.l2(l2_factor),
+                                 trainable = True)
+        self.w_psi = self.add_weight(shape = (2*2*max_power,), 
+                                 initializer = keras.initializers.GlorotNormal(), 
+                                 constraint = tf.keras.constraints.NonNeg(), 
+                                 regularizer = keras.regularizers.l2(l2_factor),
+                                 trainable = True)  
+        self.activation_exp = keras.layers.Lambda(lambda x: activation_exp(x, b=-1.0))
 
-    def call(self, tensor):
-        identity = self.identity(tensor)
-        exponential = self.exponential(tensor)
-        return keras.layers.concatenate([identity, exponential], axis=1)
+    def call(self, I1, I2):
+        
+        # Compute invariants in reference
+        I1_ref = keras.layers.Lambda(lambda x: x-3)(I1)
+        I2_ref = keras.layers.Lambda(lambda x: x-3)(I2)
 
+        # Raise reference invariants to powers      
+        I1_powers = tf.math.pow(I1_ref, self.powers)
+        I2_powers = tf.math.pow(I2_ref, self.powers)
+        powers = tf.concat(I1_powers, I2_powers,0)
+        
+        # Multiply by weights
+        powers_identity = keras.layers.Lambda(lambda x: x[0]*x[1])(powers, self.w_identity)
+        powers_exp = keras.layers.Lambda(lambda x: x[0]*x[1])(powers, self.w_exp)
+        
+        # Apply activation functions to powers of invariants
+        powers_exp = self.activation_exp(powers_exp)
+
+        # Concat results
+        active_results = tf.concat(powers_identity, powers_exp, 0)
+
+        # Multiply results with weights and add to strain energy tf.tensordot
+        return tf.tensordot([active_results, self.w_psi], 0)
+        
+        
 
 class MultiplyBlock(keras.layers.Layer):
     """
